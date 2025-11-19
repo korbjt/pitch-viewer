98aaf6653b3a5b37e9b3e065ed66aedc0f418b4c
John Korb <john@korb.email>
2025-11-19 00:47:39 -0500

    Initial gh-pages dist deployment

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    customSyntax: 'postcss-scss',
-    rules: {
-        'block-no-empty': true,
-        'color-hex-length': 'short',
-        'color-no-invalid-hex': true
-    }
-};
